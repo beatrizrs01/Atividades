35035a91b0443ba7b73bac12301127f98bcebff4
beatrizrs01 <165097221+beatrizrs01@users.noreply.github.com>
2024-06-27 19:55:20 -0300

    Delete PHP_beatriz/Aula 05 directory

--- PHP_beatriz/Aula 05/cod2.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php
-            echo "O valor de CAMPO 1 é: " . $_POST["campo1"];
-            echo "<br> O valor de CAMPO 2 é: " . $_POST["campo2"];
-        ?>
-    </body>
-</html>
